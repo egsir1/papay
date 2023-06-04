47e286af16bc1a0345d63e4dbbfdf4fd4cd81f98
egsir1 <egsir111@gmail.com>
2023-06-04 23:37:19 +0900

    feat: [11] Product-Controller

--- controllers/productController.js
+++ controllers/productController.js
@@ -12,9 +12,8 @@ productController.getAllProducts = async (req, res) => {
 productController.addNewProduct = async (req, res) => {
   try {
     console.log("POST: cont/addNewProduct");
-    console.log(req.member);
     //TODO: product creation develop
-    // res.json({ test: "ok" });
+    res.send("okkk");
   } catch (err) {
     console.log(`ERROR: cont/addNewProduct, ${err.message}`);
     //res.json({ state: "fail", message: err.message });
--- schema/member.model.js
+++ schema/member.model.js
@@ -22,12 +22,8 @@ const memberSchema = new mongoose.Schema(
       select: false,
     },
     mb_type: {
-<<<<<<< HEAD
       type: String,
-      // required: false,
-=======
       required: false,
->>>>>>> abf7c4f08a5f52718dcded0aa3c1960c2fe27d1f
       default: "USER",
       enum: {
         values: member_type_enums,
@@ -35,43 +31,14 @@ const memberSchema = new mongoose.Schema(
       },
     },
     mb_status: {
-<<<<<<< HEAD
       type: String,
-      // required: false,
-=======
       required: false,
->>>>>>> abf7c4f08a5f52718dcded0aa3c1960c2fe27d1f
       default: "ACTIVE",
       enum: {
         values: member_status_enums,
         message: "{VALUE} is not among permitted values",
       },
     },
-<<<<<<< HEAD
-    // mb_full_name: {
-    //   type: String,
-    //   required: false,
-    // },
-    mb_address: {
-      type: String,
-      // required: false,
-    },
-    mb_description: {
-      type: String,
-      // required: false,
-    },
-    mb_image: {
-      type: String,
-      // required: false,
-    },
-    mb_point: {
-      type: Number,
-      // required: false,
-    },
-    mb_top: {
-      type: String,
-      // required: false,
-=======
     mb_full_name: {
       type: String,
       required: false,
@@ -95,7 +62,6 @@ const memberSchema = new mongoose.Schema(
     mb_top: {
       type: String,
       required: false,
->>>>>>> abf7c4f08a5f52718dcded0aa3c1960c2fe27d1f
       default: "N",
       enum: {
         values: ordenery_enums,
@@ -104,38 +70,22 @@ const memberSchema = new mongoose.Schema(
     },
     mb_views: {
       type: Number,
-<<<<<<< HEAD
-      // required: false,
-=======
       required: false,
->>>>>>> abf7c4f08a5f52718dcded0aa3c1960c2fe27d1f
       default: 0,
     },
     mb_likes: {
       type: Number,
-<<<<<<< HEAD
-      // required: false,
-=======
       required: false,
->>>>>>> abf7c4f08a5f52718dcded0aa3c1960c2fe27d1f
       default: 0,
     },
     mb_follow_cnt: {
       type: Number,
-<<<<<<< HEAD
-      // required: false,
-=======
       required: false,
->>>>>>> abf7c4f08a5f52718dcded0aa3c1960c2fe27d1f
       default: 0,
     },
     mb_subscriber_cnt: {
       type: Number,
-<<<<<<< HEAD
-      // required: false,
-=======
       required: false,
->>>>>>> abf7c4f08a5f52718dcded0aa3c1960c2fe27d1f
       default: 0,
     },
   },
