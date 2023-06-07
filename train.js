cfc3d48b44aa25316eebb23eb32a8b7376a2055d
egsir1 <egsir111@gmail.com>
2023-06-08 00:25:41 +0900

    feat: Task-F solved

--- train.js
+++ train.js
@@ -1,1 +1,20 @@
 console.log("This is train.js");
+
+/**
+ *   F-Task: Shunday function tuzing, unga string argument pass bolsin.
+ *  Function ushbu agrumentdagi faqat digitlarni yangi stringda return qilsin!
+ */
+const data = "ad5we34jkf898877";
+const findDigits = (input) => {
+  const splitData = input.split("");
+  const filteredValue = splitData.filter((item) => {
+    for (let i = 0; i <= 9; i++) {
+      if (item == i) {
+        return item;
+      }
+    }
+  });
+  return filteredValue.join("");
+};
+const result = findDigits(data);
+console.log("Natija:", result);
