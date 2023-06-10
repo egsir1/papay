c55b734d641c5f2ab169d344e09052e7a93347e9
egsir1 <egsir111@gmail.com>
2023-06-10 10:33:53 +0900

    feat: [14] Product-Schema Model=> getMyRestaurantProducts api

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -1,14 +1,18 @@
 const Member = require("../models/Member");
+const Product = require("../models/Product");
 
 const restaurantController = module.exports;
 
-restaurantController.getMyRestaurantData = async (req, res) => {
+restaurantController.getMyRestaurantProducts = async (req, res) => {
   try {
-    console.log("GET: cont/getMyRestaurantData");
+    console.log("GET: cont/getMyRestaurantProducts");
 
     //TODO: Get my restaurant products
+    console.log("res-locals from res-cont::", res.locals);
 
-    res.render("restaurant-menu");
+    const product = new Product();
+    const data = await product.getAllProductsDataResto(res.locals.member);
+    res.render("restaurant-menu", { restaurant_data: data });
   } catch (err) {
     console.log(`ERROR: cont/getMyRestaurantData, ${err.message}`);
     res.json({ state: "fail", message: err.message });
